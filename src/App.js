ffbcb740ca9078d60ee1de72dcd293b6d68608df
Michal Heimrath <mheimrath1994@gmail.com>
2021-02-22 21:35:56 +0100

    kinda did iteration 5

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import 'bulma/css/bulma.css';
 import Navbar from './navbar/Navbar'
 import FormField from './formfield/FormField'
 import CoolButton from './coolbutton/CoolButton'
+import Message from './message/Message'
+
 
 const App = () => {
   return (
@@ -11,7 +13,10 @@ const App = () => {
       <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
       <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
       <FormField label="Password" type="password" placeholder="e.g. top secret" />
-      <CoolButton isSmall isSuccess className="button is-success" >Submit</CoolButton>
+      <CoolButton className="button is-success" >Submit</CoolButton>
+      <Message title='Hello World'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>.
+      </Message>
     </div>
   )
 };
